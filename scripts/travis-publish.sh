96678d440be567ba4e80dabdfde75f3a5f5c034d
Miles Sabin <miles@milessabin.com>
2016-02-11 16:08:34 +0000

    travis_wait swallows build output.

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -3,11 +3,13 @@
 # Example setting to use at command line for testing:
 # export TRAVIS_SCALA_VERSION=2.10.5;export TRAVIS_PULL_REQUEST="false";export TRAVIS_BRANCH="master"
 
+SBT=sbt ++${TRAVIS_SCALA_VERSION}
+
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" &&
       "${TRAVIS_BRANCH}" == "master" &&
       $(cat version.sbt) =~ "-SNAPSHOT"
 ]]; then
-  sbt ++${TRAVIS_SCALA_VERSION} validateJVM validateJS publish ;
+  ${SBT} validateJVM && ${SBT} validateJS && ${SBT} publish ;
 else
-  sbt ++${TRAVIS_SCALA_VERSION} validateJVM validateJS ;
+  ${SBT} validateJVM && ${SBT} validateJS ;
 fi
